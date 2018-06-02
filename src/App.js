e15877f5202975cdefa4ee149b08ddb87ff0f192
anderspollack <pollack.anders@gmail.com>
2018-06-01 20:47:08 -0700

    Add Project display functionality with icons to Collection list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 const classNames = require('classnames');
+const data = {
+  projects: [
+    {
+      id: 0,
+      heading: 'The Chicago Council on Science and Technology',
+      body: 'Graphic design and web development for a science outreach nonprofit organization',
+      directory: [
+	{
+	  name: 'C2ST.org Website',
+	  tags: ['Icon-ux', 'Icon-code'],
+	}
+      ],
+    },
+    {
+      id: 1,
+      heading: 'Tuition.io',
+      body: 'UX and brand collateral for a student loan repayment benefit platform',
+      directory: [],
+    },
+    {
+      id: 2,
+      heading: 'Museum in Trust',
+      body: 'Web design and development for Chicago artist Don Pollack',
+      directory: [],
+    },
+  ],
+};
 
 class Scroller extends Component {
   constructor(props) {
@@ -75,18 +102,93 @@ class Moon extends Component {
   }
 }
 
-class Card extends Component {
+class Directory extends Component {
   render() {
-    let ClassList = classNames(
+    const listings = this.props.directory.map(listing => {
+      return (
+	<li>
+	    {listing.name}
+	    <br />
+	    {listing.tags.map(tag => `${tag}, `)}
+	</li>
+      );
+    });
+    
+    return (
+      <div className="Directory">
+	  <ul>{listings}</ul>
+      </div>
+    );
+  }
+}
+
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.expand = this.expand.bind(this);
+    this.state = {
+      expanded: false,
+    };
+  }
+
+  expand() {
+    this.setState({
+      expanded: !this.state.expanded,
+    });
+    console.log(this.state.expanded);
+  }
+  
+  render() {
+    const classList = classNames(
       'Card',
       'Button',
+      { 'Expanded': this.state.expanded },
     );
     return (
-      <a href="#" className={ClassList}>
-      <h3>{ this.props.heading }</h3>
-      <p>{ this.props.body }</p>
-      </a>
-    )
+      <li>
+	  <a
+	   href="#"
+	   className={classList}
+	   onClick={this.expand}>
+	      <h3>{ this.props.heading }</h3>
+	      <p>{ this.props.body }</p>
+	      <Directory directory={ this.props.directory } />
+	  </a>
+      </li>
+    );
+  }
+}
+
+class Container extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: data.projects,
+    }
+  }
+  
+  render() {
+    const cards = this.state.cards.map(card => {
+      return (
+	<Card
+	 id={card.id}
+	 heading={card.heading}
+	 body={card.body}
+	 directory={card.directory}
+	 onclick={() => this.setState({ selected: card.id })} /> 
+      );
+    });
+    
+    return (
+      <div className="Container">
+	  <div className="DataHeading">
+	      <h2>Recent Work</h2>
+	  </div>
+	  <ul>{cards}</ul>
+	  <Moon />
+	  <div className="Shadow"></div>
+      </div>
+    );
   }
 }
 
@@ -94,27 +196,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-	  {/* 	  <div className="Sea"></div> */}
 	  <World />
-	  <div className="Container">
-	      <div className="DataHeading">
-		  <h2>Recent Work</h2>
-	      </div>
-	      <Card
-	       heading="The Chicago Council on Science and Technology"
-	       body="Graphic design and web development for a science outreach nonprofit organization"
-	      />
-	      <Card
-	       heading="Tuition.io"
-	       body="UX and brand collateral for a student loan repayment benefit platform"
-	      />
-	      <Card
-	       heading="Museum in Trust"
-	       body="Web design and development for Chicago artist Don Pollack "
-	      />
-	      <Moon />
-	      <div className="Shadow"></div>
-	  </div>
+	  <Container />
       </div>
     );
   }
