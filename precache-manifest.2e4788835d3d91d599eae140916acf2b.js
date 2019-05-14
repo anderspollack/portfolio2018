24719005d1996b6f8ad04f4129cd7c96f2a6c7ab
anderspollack <pollack.anders@gmail.com>
2019-05-13 18:56:30 -0700

    Updates

---
+++ precache-manifest.2e4788835d3d91d599eae140916acf2b.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35c7773278418b5b8fe1b76df777736",
+    "url": "/portfolio2018/index.html"
+  },
+  {
+    "revision": "63d30590ac8ce8980919",
+    "url": "/portfolio2018/static/css/main.86f09c90.chunk.css"
+  },
+  {
+    "revision": "4b8b8340bff5455531b0",
+    "url": "/portfolio2018/static/js/2.20e7f9bd.chunk.js"
+  },
+  {
+    "revision": "63d30590ac8ce8980919",
+    "url": "/portfolio2018/static/js/main.25e147a6.chunk.js"
+  },
+  {
+    "revision": "721b221120205a5a59c4",
+    "url": "/portfolio2018/static/js/runtime~main.aa9ab21d.js"
+  },
+  {
+    "revision": "80600ca5d8b43551bfed3b491a617ab5",
+    "url": "/portfolio2018/static/media/business-card-back.80600ca5.jpg"
+  },
+  {
+    "revision": "990ed03630deb2cc270ce18d240298c6",
+    "url": "/portfolio2018/static/media/business-card-front.990ed036.jpg"
+  },
+  {
+    "revision": "88b63e6ccd133f2a79f5aaeaac6f23e0",
+    "url": "/portfolio2018/static/media/business-size-one-pager-1.88b63e6c.pdf"
+  },
+  {
+    "revision": "dff702ca54e50b89b92b0d16f7514704",
+    "url": "/portfolio2018/static/media/business-size-one-pager-1.dff702ca.jpg"
+  },
+  {
+    "revision": "4ef79fa34b9c0b3d58e652d9399085f6",
+    "url": "/portfolio2018/static/media/business-size-one-pager-2.4ef79fa3.jpg"
+  },
+  {
+    "revision": "500415ae9ece3df25a2e078389ab23d6",
+    "url": "/portfolio2018/static/media/business-size-one-pager-2.500415ae.pdf"
+  },
+  {
+    "revision": "52a3b11a0ff0905bde291aa352996f13",
+    "url": "/portfolio2018/static/media/business-size-one-pager-3.52a3b11a.pdf"
+  },
+  {
+    "revision": "ae1f874b63d3eca41b5bb545a088990a",
+    "url": "/portfolio2018/static/media/business-size-one-pager-3.ae1f874b.jpg"
+  },
+  {
+    "revision": "2671ea2621b345ba72ffc079aacb5fd3",
+    "url": "/portfolio2018/static/media/c2st-website-desktop.2671ea26.jpg"
+  },
+  {
+    "revision": "d22e66f2955af5a00bd790c677bf9003",
+    "url": "/portfolio2018/static/media/c2st-website-mobile.d22e66f2.png"
+  },
+  {
+    "revision": "3cfd65066d3786e539fc7bc437bc01b2",
+    "url": "/portfolio2018/static/media/census-one-pager.3cfd6506.jpg"
+  },
+  {
+    "revision": "ee309e5feeeb5dfc5e631cf864e57ef9",
+    "url": "/portfolio2018/static/media/census-one-pager.ee309e5f.pdf"
+  },
+  {
+    "revision": "f0806a3ceaf1c42641931359eac8f183",
+    "url": "/portfolio2018/static/media/donpollack-website-desktop-old.f0806a3c.png"
+  },
+  {
+    "revision": "bdaebc568412ceff733f9c3f7cf6c437",
+    "url": "/portfolio2018/static/media/donpollack-website-desktop.bdaebc56.jpg"
+  },
+  {
+    "revision": "85bc5812f2d31e5d4cb73f2e6bdbcaf3",
+    "url": "/portfolio2018/static/media/donpollack-website-mobile.85bc5812.jpg"
+  },
+  {
+    "revision": "291b6170f875945262afbee472c0c11d",
+    "url": "/portfolio2018/static/media/earth.291b6170.png"
+  },
+  {
+    "revision": "bdc519d79e0e7bd16e99d836caa4201a",
+    "url": "/portfolio2018/static/media/email-template-1-desktop.bdc519d7.png"
+  },
+  {
+    "revision": "ccdea6e7170eb34879efe82ce128177f",
+    "url": "/portfolio2018/static/media/email-template-1-mobile.ccdea6e7.png"
+  },
+  {
+    "revision": "2b24394caf987acd46358670b820542c",
+    "url": "/portfolio2018/static/media/email-template-2-desktop.2b24394c.png"
+  },
+  {
+    "revision": "04cb5aee9949d3839bfe3161f7a3915b",
+    "url": "/portfolio2018/static/media/email-template-2-mobile.04cb5aee.png"
+  },
+  {
+    "revision": "cf59ea7027f622572465ba15f0c757d2",
+    "url": "/portfolio2018/static/media/email-template-3-desktop.cf59ea70.png"
+  },
+  {
+    "revision": "e2f7bc877416d4633addf06db107f427",
+    "url": "/portfolio2018/static/media/email-template-3-mobile.e2f7bc87.png"
+  },
+  {
+    "revision": "0289cb316ed2ee9ba99c7c6ec7be4d3b",
+    "url": "/portfolio2018/static/media/employee-one-pager.0289cb31.jpg"
+  },
+  {
+    "revision": "a5ab8a34535cd3f7dd60e4fa0f1b3876",
+    "url": "/portfolio2018/static/media/employee-one-pager.a5ab8a34.pdf"
+  },
+  {
+    "revision": "bd89b99f46199af017d4d9943eab2ed9",
+    "url": "/portfolio2018/static/media/executive-summary.bd89b99f.pdf"
+  },
+  {
+    "revision": "c46bd6a3fe6b44f3cb05ee5001b68c99",
+    "url": "/portfolio2018/static/media/executive-summary.c46bd6a3.jpg"
+  },
+  {
+    "revision": "5a4306da1f913ee32bd732eb07ed7efe",
+    "url": "/portfolio2018/static/media/faq-template.5a4306da.pdf"
+  },
+  {
+    "revision": "a7926b4565c826bd9672629d8496ba02",
+    "url": "/portfolio2018/static/media/faq-template.a7926b45.jpg"
+  },
+  {
+    "revision": "86efee681107469c5ced3f4408426bd6",
+    "url": "/portfolio2018/static/media/industry-type-one-pager-1.86efee68.pdf"
+  },
+  {
+    "revision": "ee42672a40a8bdd6d52c18e830fe8e32",
+    "url": "/portfolio2018/static/media/industry-type-one-pager-1.ee42672a.jpg"
+  },
+  {
+    "revision": "1a274b3c40ef5bfb380e23bd5a383d3c",
+    "url": "/portfolio2018/static/media/industry-type-one-pager-2.1a274b3c.pdf"
+  },
+  {
+    "revision": "926364cff495ef5dce9e12644355c26d",
+    "url": "/portfolio2018/static/media/industry-type-one-pager-2.926364cf.jpg"
+  },
+  {
+    "revision": "648ddceee4bc8cbd37a941fca8afb1e7",
+    "url": "/portfolio2018/static/media/industry-type-one-pager-3.648ddcee.jpg"
+  },
+  {
+    "revision": "b3c8aac879d9d877863838c1b4621dd8",
+    "url": "/portfolio2018/static/media/industry-type-one-pager-3.b3c8aac8.pdf"
+  },
+  {
+    "revision": "18c8b49aa66dff32517dc73f36925a32",
+    "url": "/portfolio2018/static/media/newsletter-2015-spread.18c8b49a.jpg"
+  },
+  {
+    "revision": "a8b8dbe8ed48a2821cdb1e2865c21c20",
+    "url": "/portfolio2018/static/media/newsletter-2015.a8b8dbe8.pdf"
+  },
+  {
+    "revision": "183b3ccc8980b96c7c96dfdaaf37f35d",
+    "url": "/portfolio2018/static/media/newsletter-2016-spread.183b3ccc.jpg"
+  },
+  {
+    "revision": "509e4900a64d01dfcba24455dcf5f67b",
+    "url": "/portfolio2018/static/media/newsletter-2016.509e4900.pdf"
+  },
+  {
+    "revision": "3afef8f5620390c93477ce7fd59ca86e",
+    "url": "/portfolio2018/static/media/newsletter-2017-spread.3afef8f5.jpg"
+  },
+  {
+    "revision": "c2fbd2e72e35821c6a50cf6679b5b399",
+    "url": "/portfolio2018/static/media/newsletter-2017.c2fbd2e7.pdf"
+  },
+  {
+    "revision": "a9283046fddd40fd12602cc6548792ce",
+    "url": "/portfolio2018/static/media/noise.a9283046.png"
+  },
+  {
+    "revision": "fafcb476a90ba660fc5fd8d916fe2641",
+    "url": "/portfolio2018/static/media/noise@2x.fafcb476.png"
+  },
+  {
+    "revision": "7112bd4413047c383dca9a39d0a65044",
+    "url": "/portfolio2018/static/media/profitability-one-pager.7112bd44.jpg"
+  },
+  {
+    "revision": "ee702593f956a557dd0c47f33e2e6e1f",
+    "url": "/portfolio2018/static/media/profitability-one-pager.ee702593.pdf"
+  },
+  {
+    "revision": "18168ef645f7b4179d1bf0325b0ea774",
+    "url": "/portfolio2018/static/media/sales-faq.18168ef6.jpg"
+  },
+  {
+    "revision": "65fe4a0bee1a577dbb06cb8266e23176",
+    "url": "/portfolio2018/static/media/sales-faq.65fe4a0b.pdf"
+  },
+  {
+    "revision": "7a82bc7126c0bb7a6dadf1ea6be1a448",
+    "url": "/portfolio2018/static/media/screenshot.7a82bc71.png"
+  },
+  {
+    "revision": "c10129b1773c208f28cca2c40b8017f8",
+    "url": "/portfolio2018/static/media/wellness-one-pager.c10129b1.jpg"
+  },
+  {
+    "revision": "650cee5e3c0b84e6b58256d27a909767",
+    "url": "/portfolio2018/static/media/wellness–one-pager.650cee5e.pdf"
+  }
+]);
